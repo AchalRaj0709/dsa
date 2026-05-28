d606c8935a84c865672c976130f7c3218e94c56e
AchalRaj0709 <rajachalp782@gmail.com>
2026-05-28 06:54:48 +0530

    linked list

--- LinkedList.java
+++ LinkedList.java
@@ -120,6 +120,23 @@ public class LinkedList {
         return -1; //key not found
     }
 
+    public int helper(Node head,int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next, key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
         ll.addFirst(2);
@@ -129,8 +146,8 @@ public class LinkedList {
         ll.add(2, 3);
         ll.print();
 
-        System.out.println(ll.itrSearch(3));
-        System.out.println(ll.itrSearch(10));
+        System.out.println(ll.recSearch(3));
+        System.out.println(ll.recSearch(10));
 
     }
 
