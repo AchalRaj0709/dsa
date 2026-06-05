b614fd97e64df2a60b3dda5f925b11cf4ecfa77d
AchalRaj0709 <rajachalp782@gmail.com>
2026-06-05 14:51:12 +0530

    zigZag Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -178,7 +178,7 @@ public class LinkedList {
     //Slow-Fast approach
     public Node findMid(Node head){
         Node slow=head;
-        Node fast=head;
+        Node fast=head.next; //last node of first half
 
         while(fast !=null && fast.next !=null){
             slow=slow.next; //+1
@@ -232,15 +232,104 @@ public class LinkedList {
         return false;
     }
 
+    public Node merge(Node head1,Node head2){
+        Node mergedLL=new Node(-1);
+        Node temp=mergedLL;
+
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+            while(head1 != null){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            while(head2 != null){
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+            
+        }
+        return mergedLL.next;
+    }
+
+    public  Node mergeSort(Node head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        //find mid
+        Node mid=findMid(head);
+        //left and right Merge Sort
+        Node rightHead=mid.next;
+        mid.next=null;
+
+        Node newLeft=mergeSort(head);
+        Node newRight=mergeSort(rightHead);
+
+        //merge
+        return merge(newLeft,newRight);
+    }
+
+    public void zigZag(){
+        //find mid 
+        Node slow=head;
+        Node fast=head.next;
+        while(fast != null && fast.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=slow;
+
+        //reverse 2nd half
+        Node curr=mid.next;
+        mid.next=null;
+        Node prev=null;
+        Node next;
+
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        Node left =head;
+        Node right=prev;
+        Node nextL,nextR;
+
+        //alternate merge -zigzag merge
+        while(left !=null && right !=null){
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+
+            left=nextL;
+            right=nextR;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
         ll.addLast(1);
         ll.addLast(2);
-        ll.addLast(2);
-        ll.addLast(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
         ll.print();
 
-        System.out.println(ll.checkPallindrome());
+        ll.zigZag();
+        ll.print();
+
+       
     }
 
     
